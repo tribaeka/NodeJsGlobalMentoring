c13600791dadb0503ccd7fc28b386879be7d9925
Yahor.Hlushak <Yahor.Hlushak@wolterskluwer.com>
2021-03-09 16:39:46 +0300

    homework-1: task 1.2 refactoring

--- task-1-2/index.js
+++ task-1-2/index.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 const csv = require('csvtojson');
+const { pipeline } = require('stream');
+
 
 const csvFilePath = path.resolve(__dirname, 'csv/data.csv');
 const txtFilePath = path.resolve(__dirname, 'data.txt');
@@ -8,4 +10,38 @@ const txtFilePath = path.resolve(__dirname, 'data.txt');
 const readStream = fs.createReadStream(csvFilePath);
 const writeStream = fs.createWriteStream(txtFilePath);
 
-readStream.pipe(csv()).pipe(writeStream);
+const parseCsv = () => {
+    csv()
+        .fromFile(csvFilePath)
+        .then((jsonObj) => {
+            let hasError = false;
+            jsonObj.forEach(item => {
+                fs.appendFile(txtFilePath, `${JSON.stringify(item)}\n`, err => {
+                    if (err) {
+                        console.error('Failed to modify data.txt.', err);
+                        hasError = true;
+                    }
+                })
+                if (!hasError) {
+                    console.log('data.txt created.');
+                }
+            })
+        });
+};
+const parseCsvWithPipeline = () => {
+    pipeline(
+        readStream,
+        csv(),
+        writeStream,
+        err => {
+            if (err) {
+                console.error('Failed to create data.txt.', err);
+            } else {
+                console.log('data.txt created.');
+            }
+        }
+    );
+};
+
+parseCsv();
+
