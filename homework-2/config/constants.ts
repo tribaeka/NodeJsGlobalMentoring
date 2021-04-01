dc2448351a97a5798aadb7741017d61e73f136ba
Yahor.Hlushak <Yahor.Hlushak@wolterskluwer.com>
2021-04-01 19:55:09 +0300

    homework-2: refactoring

--- homework-2/config/constants.ts
+++ homework-2/config/constants.ts
@@ -1,3 +1,5 @@
+import { User } from "../interfaces/User";
+
 export const APP_PORT = 3000;
 export const RES_STATUS_CODES = {
     BAD_REQUEST: 400,
@@ -10,4 +12,34 @@ export const RES_MESSAGES = {
     NOT_FOUND: 'Not Found',
     CREATED: 'Created',
     NO_CONTENT: 'No Content'
-}
+};
+export const initialUsersList: User[] = [
+    {
+        id: '14f4e1a1-ff87-4f93-b01f-2293f82f1345',
+        login: 'user1query',
+        password: 'user1',
+        age: 1,
+        isDeleted: false
+    },
+    {
+        id: 'cbe11495-1dc1-489b-9a1b-5d215e6501a9',
+        login: 'user2query',
+        password: 'user2',
+        age: 2,
+        isDeleted: false
+    },
+    {
+        id: 'fe1ed5a7-7c47-41fe-9248-f776bd706b18',
+        login: 'user3query',
+        password: 'user3',
+        age: 3,
+        isDeleted: false
+    },
+    {
+        id: '4b4ba4ab-0f8c-4669-9aef-0bcf220dad8e',
+        login: 'user4',
+        password: 'user4',
+        age: 4,
+        isDeleted: false
+    }
+];
--- homework-2/services/userService.ts
+++ homework-2/services/userService.ts
@@ -1,39 +1,9 @@
 import { User } from "../interfaces/User";
 import { v4 as uuidv4 } from 'uuid';
-
-export const defaultUsersList: User[] = [
-    {
-        id: '14f4e1a1-ff87-4f93-b01f-2293f82f1345',
-        login: 'user1query',
-        password: 'user1',
-        age: 1,
-        isDeleted: false
-    },
-    {
-        id: 'cbe11495-1dc1-489b-9a1b-5d215e6501a9',
-        login: 'user2query',
-        password: 'user2',
-        age: 2,
-        isDeleted: false
-    },
-    {
-        id: 'fe1ed5a7-7c47-41fe-9248-f776bd706b18',
-        login: 'user3query',
-        password: 'user3',
-        age: 3,
-        isDeleted: false
-    },
-    {
-        id: '4b4ba4ab-0f8c-4669-9aef-0bcf220dad8e',
-        login: 'user4',
-        password: 'user4',
-        age: 4,
-        isDeleted: false
-    }
-];
+import { initialUsersList } from "../config/constants";
 
 export default class UserService {
-    private static usersList = defaultUsersList;
+    private static usersList = initialUsersList;
 
     static getAllUsers(): User[] {
         return UserService.usersList;
