3cb5b798946050ff81f69291d8dbbb800b67e120
Yahor.Hlushak <Yahor.Hlushak@wolterskluwer.com>
2021-05-30 12:14:50 +0300

    homework-5: fix magic numbers

--- homework-2/middewares/executionTimeLogMiddleware.ts
+++ homework-2/middewares/executionTimeLogMiddleware.ts
@@ -1,12 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 import LogService from "../services/logService";
 
+const SECOND_IN_MS = 1000;
+const SECOND_IN_NANO_S = 1e6;
+
 export function executionTimeLogMiddleware(req: Request, res: Response, next: NextFunction): void {
     const startHrTime = process.hrtime();
 
     res.on("finish", () => {
         const elapsedHrTime = process.hrtime(startHrTime);
-        const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6;
+        const elapsedTimeInMs = elapsedHrTime[0] * SECOND_IN_MS + elapsedHrTime[1] / SECOND_IN_NANO_S;
 
         LogService.logResponseInfo(req, res, elapsedTimeInMs);
     });
